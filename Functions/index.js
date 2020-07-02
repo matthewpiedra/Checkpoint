f01185045ca99ad3e91e9a46ab91af1873d6aabd
unknown <matthewpiedra1228@gmail.com>
2020-07-02 15:25:27 -0400

    Finished with Functions section

--- Functions/index.js
+++ Functions/index.js
@@ -28,6 +28,7 @@ CREATE A FUNCTION THAT RETURNS THE BOOLEAN false
 
 function firstFunction() {
   // YOUR CODE HERE
+  return false;
 }
 
 /*
@@ -39,6 +40,7 @@ CREATE A FUNCTION THAT RETURNS THE STRING 'hello world'
 
 function helloWorld() {
   // YOUR CODE HERE
+  return 'hello world';
 }
 
 /*
@@ -55,6 +57,7 @@ YOU WILL HAVE TO EDIT THE FUNCTION DECLARATION (the line where the key word 'fun
 
 function question3(param) {
   // YOUR CODE HERE
+  return param;
 }
 
 /*
@@ -70,6 +73,7 @@ concatQuestion('blake') ===> 'hello blake'
 
 function concatQuestion(str) {
   // YOUR CODE HERE
+  return `hello ${str}`;
 }
 
 /*
@@ -86,6 +90,7 @@ subtraction(1, 2) ===> -1
 
 function subtraction(num1, num2) {
   // YOUR CODE HERE
+  return num1 - num2;
 }
 
 /*
@@ -102,6 +107,7 @@ multiplication(2, 0) ===> 0
 
 function multiplication(num1, num2) {
   // YOUR CODE HERE
+  return num1 * num2;
 }
 
 /*
@@ -118,6 +124,7 @@ square(4) ===> 16
 
 function square(num) {
   // YOUR CODE HERE
+  return num * num;
 }
 
 /*
@@ -135,6 +142,7 @@ var answerToQuestion8;
 
 function question8() {
   // YOUR CODE HERE
+  return 'we can save return values in variables';
 }
 
 answerToQuestion8 = question8();
